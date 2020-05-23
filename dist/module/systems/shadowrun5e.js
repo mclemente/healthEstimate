c1818f17f28654a96ae84ab40dbd26742c66cac6
Shylight <tsuki.no.mai@live.com>
2020-05-23 21:18:22 +0300

    fix Shadowrun

--- dist/module/systems/shadowrun5e.js
+++ dist/module/systems/shadowrun5e.js
@@ -1,5 +1,6 @@
 const fraction = function (token) {
-	const [stun, physical] = token.actor.data.data.track
+	const stun = token.actor.data.data.track.stun
+	const physical = token.actor.data.data.track.physical
 	return Math.min(
 		(stun.max - stun.value) / stun.max,
 		(physical.max - physical.value) / physical.max
