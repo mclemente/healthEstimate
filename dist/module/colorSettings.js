d6516991a3e23c7ef9b63d138321a0002d70b02c
Shylight <tsuki.no.mai@live.com>
2020-06-04 21:42:45 +0300

    Move gradient implemenation into the class

--- dist/module/colorSettings.js
+++ dist/module/colorSettings.js
@@ -2,6 +2,9 @@ export class HealthEstimateColorSettings extends FormApplication {
 	
 	constructor(object, options = {}) {
 		super(object, options)
+		console.log(this)
+		this.initHooks()
+		
 	}
 	
 	/**
@@ -14,11 +17,39 @@ export class HealthEstimateColorSettings extends FormApplication {
 			template:      "./modules/healthEstimate/templates/settings.hbs",
 			classes:       ["sheet"],
 			width:         600,
+			height:        300,
 			closeOnSubmit: true
 		})
 	}
 	
 	getData(options) {
 		return {}
+	}
+	
+	initHooks() {
+		Hooks.on('renderHealthEstimateColorSettings', () => {
+			console.log("owo")
+			this.gradEx = document.getElementById("gradientExampleHE")
+			this.gp     = new Grapick({el: '#gradientControlsHE'})
+			this.gp.addHandler(0, "#f00")
+			this.gp.addHandler(100, "#00f")
+			this.gp.on('change', complete => {
+				this.updateGradient(
+					this.gp.handlers.map(a => a.color),
+					this.gp.handlers.map(a => Math.round(a.position) / 100)
+				)
+			})
+			this.updateGradient()
+		})
+	}
+	
+	updateGradient(colors = ["#f00", "#00f"], locations = [0, 1]) {
+		let gradString = ''
+		for (let i = 0; i < 100; i++) {
+			gradString += `<span style="display:inline-block;height:25px;width:1%;background-color:${
+				(chroma.bezier(colors).scale().domain(locations).mode('lch'))(i / 100).hex()
+			};"></span>`
+		}
+		this.gradEx.innerHTML = gradString
 	}
 }
