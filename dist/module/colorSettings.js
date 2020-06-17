e8bb7d193ed1deeb70ac270f58be6efd642f8655
Shylight <tsuki.no.mai@live.com>
2020-06-17 23:18:11 +0300

    Add more settings and fix a bug in grapick
    No idea what was causing it, but removing a helper function that was failing to initialize sure helped.

--- dist/module/colorSettings.js
+++ dist/module/colorSettings.js
@@ -4,9 +4,10 @@ export class HealthEstimateColorSettings extends FormApplication {
 
 	constructor (object, options = {}) {
 		super(object, options)
-		this.gradFn = new Function()
+		this.gradFn     = new Function()
 		this.gradColors = []
-		this.initHooks()
+		Hooks.once('renderHealthEstimateColorSettings', this.initHooks.bind(this))
+		Hooks.once('closeHealthEstimateColorSettings', () => {delete this.gp})
 	}
 
 	/**
@@ -19,7 +20,7 @@ export class HealthEstimateColorSettings extends FormApplication {
 			template     : './modules/healthEstimate/templates/settings.hbs',
 			classes      : ['sheet'],
 			width        : 600,
-			height       : 300,
+			height       : 800,
 			closeOnSubmit: true
 		})
 	}
@@ -28,82 +29,104 @@ export class HealthEstimateColorSettings extends FormApplication {
 		function path (key) {
 			return `core.colorSettings.${key}`
 		}
-		function prepSelection(key) {
-			let data = settingData(path(key))
-			let current = sGet(path(key))
-			let result = []
-			for (let [k,v] of Object.entries(data.choices)) {
-				result.push({
-					key: k,
-					value: v,
+
+		function prepSelection (key, param = false) {
+			let data    = settingData(path(key))
+			let current = ''
+			let result  = {
+				select: [],
+				name  : data.name,
+				hint  : data.hint
+			}
+
+			if (param) {
+				let currentObject = sGet(path(key))
+				current           = currentObject[param]
+				// for (let [k, v] of Object.entries((currentObject))) {
+				// 	result[k] = v
+				// }
+				Object.assign(result, currentObject)
+			} else {
+				current = sGet(path(key))
+			}
+
+			for (let [k, v] of Object.entries(data.choices)) {
+				result.select.push({
+					key     : k,
+					value   : v,
 					selected: k === current
 				})
 			}
 			return result
 		}
+
 		function prepSetting (key) {
 			let data = settingData(path(key))
 			return {
 				value: sGet(path(key)),
-				name: data.name,
-				hint: data.hint
+				name : data.name,
+				hint : data.hint
 			}
 		}
 
 		return {
-			useColor        : prepSetting('color'),
-			smoothGradient  : prepSetting('smoothGradient'),
-			bezier          : prepSetting('bezier'),
-			deadColor       : prepSetting('deadColor'),
-			mode            : prepSelection('mode'),
-			outline         : prepSelection('outline')
+			useColor      : prepSetting('color'),
+			smoothGradient: prepSetting('smoothGradient'),
+			bezier        : prepSetting('bezier'),
+			deadColor     : prepSetting('deadColor'),
+			mode          : prepSelection('mode'),
+			outline       : prepSelection('outline', 'mode')
 		}
 	}
 
 	initHooks () {
-		Hooks.on('renderHealthEstimateColorSettings', () => {
-			const gradientPositions = game.settings.get(`healthEstimate`, `core.colorSettings.gradient`)
-			const bezier            = document.getElementById(`bezier`)
-			const mode              = document.getElementById(`mode`)
+		const gradientPositions = game.settings.get(`healthEstimate`, `core.colorSettings.gradient`)
+		const bezier            = document.getElementById(`bezier`)
+		const mode              = document.getElementById(`mode`)
+		const deadColor         = document.getElementById('deadColor')
+		console.warn (this)
 
-			this.smoothGradient = document.getElementById('smoothGradient')
-			this.gradEx = document.getElementById('gradientExampleHE')
-			this.gp     = this.gp || new Grapick({
-				el: '#gradientControlsHE',
-				colorEl: '<input id="colorpicker"/>'
-			})
-			this.gp.setColorPicker(handler => {
-				const el = handler.getEl().querySelector('#colorpicker');
-				console.log(el)
+		this.smoothGradient = document.getElementById('smoothGradient')
+		this.gradEx         = document.getElementById('gradientExampleHE')
+		this.gp             = new Grapick({
+			el     : '#gradientControlsHE',
+			colorEl: '<input id="colorpicker"/>'
+		})
+		this.gp.setColorPicker(handler => {
+			const el = handler.getEl().querySelector('#colorpicker')
 
-				$(el).spectrum({
-					color    : handler.getColor(),
-					showAlpha: true,
-					change (color) {
-						handler.setColor(color.toRgbString());
-					},
-					move (color) {
-						handler.setColor(color.toRgbString(), 0);
-					}
-				});
+			$(el).spectrum({
+				color    : handler.getColor(),
+				showAlpha: true,
+				change (color) {
+					handler.setColor(color.toRgbString())
+				},
+				move (color) {
+					handler.setColor(color.toRgbString(), 0)
+				}
 			})
-			this.setHandlers(gradientPositions).then(() => {
+		})
+		this.setHandlers(gradientPositions).then(() => {
+			this.updateGradientFunction()
+		})
+
+		$(deadColor).spectrum({
+			color: sGet('core.colorSettings.deadColor'),
+			change (color) {
+				deadColor.value = color.toRgbString()
+			}
+		})
+
+		this.gp.on('change', complete => {
+			this.updateGradient()
+		})
+		for (const v of [bezier, mode]) {
+			v.addEventListener(`change`, () => {
 				this.updateGradientFunction()
 			})
-			// this.gp.addHandler(0, "#f00")
-			// this.gp.addHandler(100, "#00f")
-			this.gp.on('change', complete => {
-				this.updateGradient()
-			})
-			for (const v of [bezier, mode]) {
-				v.addEventListener(`change`, () => {
-					this.updateGradientFunction()
-				})
-			}
-			this.smoothGradient.addEventListener('change', () => {
-				this.updateGradient()
-			})
-
+		}
+		this.smoothGradient.addEventListener('change', () => {
+			this.updateGradient()
 		})
 	}
 
@@ -114,9 +137,9 @@ export class HealthEstimateColorSettings extends FormApplication {
 	}
 
 	updateGradientFunction () {
-		const bezier           = document.getElementById(`bezier`).checked
-		const mode             = document.getElementById(`mode`).value
-		const colorHandler     = bezier ? `bezier(colors).scale()` : `scale(colors)`
+		const bezier       = document.getElementById(`bezier`).checked
+		const mode         = document.getElementById(`mode`).value
+		const colorHandler = bezier ? `bezier(colors).scale()` : `scale(colors)`
 
 		// this.gradFn = new Function(
 		// 	`position`, `colors`, `colorPositions`,
@@ -132,9 +155,9 @@ export class HealthEstimateColorSettings extends FormApplication {
 	updateGradient () {
 		const colors         = this.gp.handlers.map(a => a.color)
 		const colorPositions = this.gp.handlers.map(a => Math.round(a.position) / 100)
-		const gradLength = this.smoothGradient.checked ? 100 : sGet('core.stateNames').split(/[,;]\s*/).length
-		const width = 100/gradLength
-		this.gradColors = this.gradFn(gradLength, colors, colorPositions)
+		const gradLength     = this.smoothGradient.checked ? 100 : sGet('core.stateNames').split(/[,;]\s*/).length
+		const width          = 100 / gradLength
+		this.gradColors      = this.gradFn(gradLength, colors, colorPositions)
 		let gradString       = ''
 
 		for (let i = 0; i < gradLength; i++) {
