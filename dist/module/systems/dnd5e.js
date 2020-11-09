3d5e9fd3ba2197b2da0e16ad038ffe30434d1567
Shylight <tsuki.no.mai@live.com>
2020-11-10 00:35:31 +0300

    Fix 5e vehicles

--- dist/module/systems/dnd5e.js
+++ dist/module/systems/dnd5e.js
@@ -4,11 +4,20 @@ import {t} from '../utils.js'
 
 const fraction = function (token) {
 	const hp    = token.actor.data.data.attributes.hp
-	let addTemp = 0
-	if (token.actor.data.type === 'character' && game.settings.get('healthEstimate', 'core.addTemp')) {
-		addTemp = 1
+	let temp = 0
+	if (token.actor.data.type === 'vehicle' && game.settings.get('healthEstimate', 'starfinder.useThreshold')){
+		if (hp.value > hp.dt) {
+			return 1
+		} else if (hp.value > 0) {
+			return 0.5
+		} else {
+			return 0
+		}
 	}
-	return Math.min((hp.temp * addTemp + hp.value) / hp.max, 1)
+	if (token.actor.data.type === 'character' && game.settings.get('healthEstimate', 'core.addTemp')) {
+		temp = hp.temp
+	}
+	return Math.min((temp + hp.value) / hp.max, 1)
 }
 const settings = () => {
 	return {
@@ -30,7 +39,7 @@ const settings = () => {
 		},
 		'starfinder.vehicleNames'  : {
 			type   : String,
-			default: t('starfinder.vehicleNames.default').join(', '),
+			default: t('dnd5e.vehicleNames.default').join(', '),
 			hint   : t('dnd5e.vehicleNames.hint'),
 		},
 	}
