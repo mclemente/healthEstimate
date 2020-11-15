06cc2a2f6fbf951d345a7e935e482f666e40c06a
Shylight <tsuki.no.mai@live.com>
2020-11-15 20:59:00 +0300

    Split 13th Age to its own file

---
+++ dist/module/systems/archmage.js
@@ -0,0 +1,28 @@
+// import {addTemp, breakOnZeroMaxHP} from './commonSettings.js'
+import {descriptions} from './starfinder.js'
+import {t} from '../utils.js'
+
+const fraction = function (token) {
+	const hp = token.actor.data.data.attributes.hp
+	let temp = 0
+	if (token.actor.data.type === 'character' && game.settings.get('healthEstimate', 'core.addTemp')) {
+		temp = hp.temp
+	}
+	return Math.min((temp + hp.value) / hp.max, 1)
+}
+const settings = () => {
+	return {
+		'core.addTemp'         : {
+			type   : Boolean,
+			default: false,
+		},
+		'core.breakOnZeroMaxHP': {
+			type   : Boolean,
+			default: true,
+		},
+	}
+}
+
+const breakCondition = `||game.settings.get('healthEstimate', 'core.breakOnZeroMaxHP') && token.actor.data.data.attributes.hp.max === 0`
+
+export {fraction, settings, breakCondition}
