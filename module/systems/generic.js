fdf6b80ebed2bf5177836852994237954a4bd4c0
mclemente <matheus.xb@gmail.com>
2021-06-28 05:06:35 -0300

    Improving System Compatibility
    Fix https://github.com/mclemente/healthEstimate/issues/24

--- module/systems/generic.js
+++ module/systems/generic.js
@@ -1,6 +1,40 @@
+import {sGet, t} from '../utils.js';
+
 const fraction = function (token) {
-	const hp = token.actor.data.data.attributes?.hp || token.actor.data.data.hp;
-	return Math.min(hp.value / hp.max, 1);
+	const hp = sGet("core.custom.FractionHP") || token.actor.data.data.attributes?.hp || token.actor.data.data.hp;
+	let temp = 0;
+	if (game.settings.get('healthEstimate', 'core.addTemp') && token.actor.data.type === 'character') {
+		temp = hp.temp;
+	}
+	const outputs = [
+		Math.min((hp.value + temp) / hp.max, 1),
+		(hp.max - hp.value) / hp.max
+	];
+	return outputs[sGet("core.custom.FractionMath")];
+};
+const settings = () => {
+	return {
+		'core.custom.FractionHP' : {
+			type: String,
+			default: ""
+		},
+		'core.custom.FractionMath' : {
+			type: Number,
+			default: 0,
+			choices: {
+				0 : t('core.custom.FractionMath.choices.0'),
+				1 : t('core.custom.FractionMath.choices.1')
+			}
+		},
+		'core.addTemp' : {
+			type   : Boolean,
+			default: false,
+		},
+		'core.breakOnZeroMaxHP' : {
+			type   : Boolean,
+			default: true,
+		}
+	};
 };
 
-export {fraction};
+export {fraction, settings};
