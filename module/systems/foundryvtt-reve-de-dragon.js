4f058e0367d8fadf2949aaea976057045790abfd
Vincent Vandemeulebrouck <vincent.vandemeulebrouck@itiviti.com>
2021-11-09 23:27:53 +0100

    reve-de-dragon support all types aof actors
    Change-Id: Ib50a5584279370548fcae3258644a44128b12bb9

--- module/systems/foundryvtt-reve-de-dragon.js
+++ module/systems/foundryvtt-reve-de-dragon.js
@@ -1,15 +1,20 @@
-const missing = {
-	value: 0,
-	max: 1,
-};
+const missing = { value: 0, max: 1, };
+
 const missingBlessures = {
 	legeres: { list: [] },
 	graves: { list: [] },
 	critiques: { list: [] },
 };
 
-function defaultIfEmpty(node, def) {
-	return node == undefined ? def : node;
+const tableBlessure = {
+	legere: [0, 20, 40, 50, 60, 70],
+	grave: [0, 60, 75],
+	critique: [0, 95],
+	inconscient: 100,
+};
+
+function betweenZeroAndOne(ratio) {
+	return Math.max(0, Math.min(ratio, 1));
 }
 
 function ratio(node) {
@@ -17,16 +22,28 @@ function ratio(node) {
 }
 
 const fraction = function (token) {
-	const endurance = defaultIfEmpty(token.actor.data.data.sante.endurance, missing);
-	const ratioEndurance = ratio(endurance);
-	if (token.actor.data.data.blessures === undefined || token.actor.data.data.sante.vie === undefined) {
-		return ratioEndurance;
+	if (token.actor.type == 'vehicule')	{
+		return 1;
 	}
+	const fatigue = token.actor.data.data.sante?.fatigue ?? missing;
+	const ratioFatigue = 1 - ratio({value: fatigue.value, max:fatigue.max}) / 2;
+	const ratioVie = ratio(token.actor.data.data.sante?.vie ?? missing);
+	const ratioEndurance = 0.4 + ratio(token.actor.data.data.sante?.endurance ?? missing) * 0.6;
+	const ratioBlessure = 1 - ratio(estimationBlessures(token));
 
-	const nodeBlessures = defaultIfEmpty(token.actor.data.data.blessures, missingBlessures);
+	return Math.min(ratioBlessure, ratioEndurance, ratioFatigue, ratioVie);
+};
+
+export { fraction };
+
+function estimationBlessures(token) {
+	if (token.actor.data.data.blessures === undefined) {
+		return missing;
+	}
+	const nodeBlessures = token.actor.data.data.blessures ?? missingBlessures;
 	const legeres = nodeBlessures.legeres.liste.filter((it) => it.active).length;
 	const graves = nodeBlessures.graves.liste.filter((it) => it.active).length;
-	const critiques = nodeBlessures.critiques.liste.filter((it) => it.active).length > 0;
+	const critiques = nodeBlessures.critiques.liste.filter((it) => it.active).length;
 
 	/*
 	 * Estimation of seriousness of wounds: considerinng wounds that can be taken.
@@ -37,29 +54,10 @@ const fraction = function (token) {
 	 * to the next (ie: 3rd serious wound becomes critical).
 	 * Using an estimation of state of health based on the worst category of wounds
 	 */
-	const tableBlessure = {
-		legere: [0, 25, 45, 60, 70, 75],
-		grave: [0, 80, 90],
-		critique: [0, 99],
-		inconscient: 100,
-	};
-	const blessures = {
+	return {
 		value: critiques > 0 ? tableBlessure.critique[critiques] : graves > 0 ? tableBlessure.grave[graves] : tableBlessure.legere[legeres],
 		max: tableBlessure.inconscient,
 	};
-
-	/*
-	 * Other values tat indicategood/bad health
-	 */
-	const ratioFatigue = 1 - ratio(token.actor.data.data.sante.fatigue, missing);
-	const ratioBlessure = 1 - ratio(blessures);
-	const ratioVie = ratio(token.actor.data.data.sante.vie);
-
-	return betweenZeroAndOne(Math.min(ratioFatigue, ratioEndurance, ratioVie, ratioBlessure));
-};
-
-export { fraction };
-
-function betweenZeroAndOne(ratio) {
-	return Math.max(0, Math.min(ratio, 1));
 }
+
+
