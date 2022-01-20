f75d1471e70f1a6dcd41195f601a65b55e74434f
mclemente <matheus.xb@gmail.com>
2022-01-20 17:00:43 -0300

    Error messages for Generic systems

--- module/systems/generic.js
+++ module/systems/generic.js
@@ -2,19 +2,21 @@ import { getNestedData, sGet, t } from "../utils.js";
 
 const fraction = function (token) {
 	const hpPath = sGet("core.custom.FractionHP");
-	let hp = getNestedData(token, sGet("core.custom.FractionHP")) || token.actor.data.data.attributes?.hp || token.actor.data.data.hp;
+	let hp = getNestedData(token, hpPath) || token.actor.data.data.attributes?.hp || token.actor.data.data.hp;
 	let temp = 0;
 	if (game.settings.get("healthEstimate", "core.addTemp") && token.actor.data.type === "character") {
 		temp = hp.temp;
 	}
+	if (hp === undefined && hpPath === "") throw new Error(`The HP is undefined, try using the ${game.i18n.localize("healthEstimate.core.custom.FractionHP.name")} setting.`);
+	else if (hp === undefined) throw new Error(`The ${game.i18n.localize("healthEstimate.core.custom.FractionHP.name")} setting ("${hpPath}") is wrong.`);
 	const outputs = [Math.min((hp.value + temp) / hp.max, 1), (hp.max - hp.value) / hp.max];
 	return outputs[sGet("core.custom.FractionMath")];
 };
 const settings = () => {
 	return {
-		'core.custom.FractionHP' : {
+		"core.custom.FractionHP": {
 			type: String,
-			default: ""
+			default: "",
 		},
 		"core.custom.FractionMath": {
 			type: Number,
