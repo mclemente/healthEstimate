59e8a5909b2e8dc4409174969f2ffe900fe46b1b
Gerald Sterling <admin@gsterling.de>
2022-05-09 21:52:09 +0200

    switched up the fraction calculation

--- module/systems/splittermond.js
+++ module/systems/splittermond.js
@@ -1,7 +1,7 @@
 const fraction = function (token) {
 	const hp = token.actor.data.data.health;
 
-    return (hp.max - hp.total.value) / hp.max;
+    return hp.total.value / hp.max;
 };
 
 export { fraction };
