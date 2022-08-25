8cd8f17e532650ace8d39beb86e1bcb76a387418
mclemente <matheus.xb@gmail.com>
2022-08-25 00:43:59 -0300

    Update starwarsffg.js
    Fix https://github.com/mclemente/healthEstimate/issues/90

--- module/systems/starwarsffg.js
+++ module/systems/starwarsffg.js
@@ -2,21 +2,13 @@ import { descriptions, t } from "../utils.js";
 
 const fraction = function (token) {
 	let hp = token.actor.system.stats.wounds;
-	if (token.actor.type === "vehicle" && game.settings.get("healthEstimate", "starfinder.useThreshold")) {
+	if (token.actor.type === "vehicle") {
 		hp = token.actor.system.stats.hullTrauma;
 	}
 	return Math.min((hp.max - hp.value) / hp.max, 1);
 };
 const settings = () => {
 	return {
-		"starfinder.useThreshold": {
-			type: Boolean,
-			default: false,
-		},
-		"starfinder.thresholdNames": {
-			type: String,
-			default: t("starfinder.thresholdNames.default"),
-		},
 		"starfinder.vehicleNames": {
 			type: String,
 			default: t("dnd5e.vehicleNames.default"),
