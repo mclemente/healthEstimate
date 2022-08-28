ab3477f65562560cce03da9e01b4b0ed8161bf56
mclemente <matheus.xb@gmail.com>
2022-08-28 16:29:47 -0300

    Revert "Update starwarsffg.js"
    This reverts commit 3fdce1aa95d7581d9cc6304ea1f43e963c365cc0.

--- module/systems/starwarsffg.js
+++ module/systems/starwarsffg.js
@@ -1,14 +1,22 @@
 import { descriptions, t } from "../utils.js";
 
 const fraction = function (token) {
-	let hp = token.actor.system.stats.wounds;
-	if (token.actor.type === "vehicle") {
-		hp = token.actor.system.stats.hullTrauma;
+	let hp = token.actor.data.data.stats.wounds;
+	if (token.actor.data.type === "vehicle" && game.settings.get("healthEstimate", "starfinder.useThreshold")) {
+		hp = token.actor.data.data.stats.hullTrauma;
 	}
 	return Math.min((hp.max - hp.value) / hp.max, 1);
 };
 const settings = () => {
 	return {
+		"starfinder.useThreshold": {
+			type: Boolean,
+			default: false,
+		},
+		"starfinder.thresholdNames": {
+			type: String,
+			default: t("starfinder.thresholdNames.default"),
+		},
 		"starfinder.vehicleNames": {
 			type: String,
 			default: t("dnd5e.vehicleNames.default"),
