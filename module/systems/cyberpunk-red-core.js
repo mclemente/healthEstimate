92a8ef635c5c1473df192d030fc81b905ea560cf
Matheus Clemente <matheus.xb@gmail.com>
2023-02-08 17:31:50 -0300

    Temporary fix for Cyberpunk RED Core
    Related to https://github.com/mclemente/healthEstimate/issues/119

--- module/systems/cyberpunk-red-core.js
+++ module/systems/cyberpunk-red-core.js
@@ -32,4 +32,5 @@ const settings = () => {
 	};
 };
 
-export { fraction, breakCondition, descriptions, settings };
+// export { fraction, breakCondition, descriptions, settings };
+export { fraction, breakCondition };
