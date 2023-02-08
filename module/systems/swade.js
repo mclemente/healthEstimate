d6762ffe629528e4013525760312f3738d97ad48
Matheus Clemente <matheus.xb@gmail.com>
2023-02-08 17:32:31 -0300

    SWADE improvements
    Related to https://github.com/mclemente/healthEstimate/issues/105

--- module/systems/swade.js
+++ module/systems/swade.js
@@ -1,10 +1,30 @@
+import { sGet } from "../utils.js";
+
 const fraction = function (token) {
 	const hp = token.actor.system.wounds;
-	let frac = (hp.max - hp.value) / hp.max;
+	let frac = 0;
+	if (token.actor.system.wildcard) {
+		const defaultWildCardMaxWounds = Math.max(sGet("swade.defaultWildCardMaxWounds"), 0);
+		frac = ((hp.max || defaultWildCardMaxWounds) - hp.value) / (hp.max || defaultWildCardMaxWounds);
+	} else frac = (hp.max - hp.value) / hp.max;
 	if (isNaN(frac)) {
-		frac = 1;
+		return 1;
 	}
+	if (frac == -Infinity) return 0;
 	return frac;
 };
 
-export { fraction };
+const tokenEffects = function (token) {
+	return token.document.overlayEffect === game.healthEstimate.deathMarker;
+};
+
+const settings = () => {
+	return {
+		"swade.defaultWildCardMaxWounds": {
+			type: Number,
+			default: 3,
+		},
+	};
+};
+
+export { fraction, settings, tokenEffects };
