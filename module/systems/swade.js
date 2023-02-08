84d36f4d4b80686dd89e987dba37db1fddcf8438
Matheus Clemente <matheus.xb@gmail.com>
2023-02-08 18:23:25 -0300

    SWADE improvements 2
    Related to https://github.com/mclemente/healthEstimate/issues/105

--- module/systems/swade.js
+++ module/systems/swade.js
@@ -1,12 +1,24 @@
-import { sGet } from "../utils.js";
+import { descriptions, sGet, t } from "../utils.js";
 
 const fraction = function (token) {
+	const type = token.actor.type;
 	const hp = token.actor.system.wounds;
-	let frac = 0;
-	if (token.actor.system.wildcard) {
-		const defaultWildCardMaxWounds = Math.max(sGet("swade.defaultWildCardMaxWounds"), 0);
-		frac = ((hp.max || defaultWildCardMaxWounds) - hp.value) / (hp.max || defaultWildCardMaxWounds);
-	} else frac = (hp.max - hp.value) / hp.max;
+	switch (type) {
+		case "npc":
+		case "vehicle":
+			if (!token.actor.system.wildcard) {
+				var frac = (hp.max - hp.value) / (1 + hp.max);
+				break;
+			}
+		// don't break if NPC is Wild Card
+		case "character":
+			const defaultWildCardMaxWounds = Math.max(sGet("swade.defaultWildCardMaxWounds"), 0);
+			const maxHP = 1 + (hp.max || defaultWildCardMaxWounds);
+			frac = (maxHP - hp.value) / maxHP;
+			break;
+		default:
+			console.warn(`Health Estimate | Token "${token.name}" has an unknown type (${token.actor.type}).`);
+	}
 	if (isNaN(frac)) {
 		return 1;
 	}
@@ -24,7 +36,12 @@ const settings = () => {
 			type: Number,
 			default: 3,
 		},
+		"starfinder.vehicleNames": {
+			type: String,
+			default: t("swade.vehicleNames.default"),
+			hint: t("swade.vehicleNames.hint"),
+		},
 	};
 };
 
-export { fraction, settings, tokenEffects };
+export { descriptions, fraction, settings, tokenEffects };
