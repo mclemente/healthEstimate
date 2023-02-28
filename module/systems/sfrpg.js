feff240273d2525dac32aea3709d6dfe0ad237fa
Matheus Clemente <matheus.xb@gmail.com>
2023-02-28 03:55:45 -0300

    #132 changes

--- module/systems/sfrpg.js
+++ module/systems/sfrpg.js
@@ -1,32 +1,39 @@
-import { descriptions } from "../utils.js";
+import { t } from "../utils.js";
 
 const fraction = function (token) {
 	const type = token.actor.type;
 	const hp = token.actor.system.attributes.hp;
 	switch (type) {
 		case "npc":
-		case "character": {
+		case "npc2":
+		case "drone":
+		case "character":
 			const sp = token.actor.system.attributes.sp;
 			const addStamina = game.settings.get("healthEstimate", "starfinder.addStamina") ? 1 : 0;
-			const temp = game.settings.get("healthEstimate", "core.addTemp") && type === "character" ? hp.temp : 0;
+			const temp = game.settings.get("healthEstimate", "core.addTemp") ? hp.temp ?? 0 : 0;
 			return Math.min((hp.value + sp.value * addStamina + temp) / (hp.max + sp.max * addStamina), 1);
-		}
-		case "vehicle": {
+		case "vehicle":
 			if (game.settings.get("healthEstimate", "starfinder.useThreshold")) {
-				if (hp.value > hp.threshold) {
-					return 1;
-				} else if (hp.value > 0) {
-					return 0.5;
-				}
+				if (hp.value > hp.threshold) return 1;
+				else if (hp.value > 0) return 0.5;
 				return 0;
 			}
+		case "starship":
 			return hp.value / hp.max;
-		}
-		case "starship": {
-			return hp.value / hp.max;
-		}
 	}
 };
+
+const descriptions = function (descriptions, stage, token, state = { dead: false, desc: "" }, fraction) {
+	const type = token.actor.type;
+	if (["starship", "vehicle", "drone"].includes(type)) {
+		descriptions = game.settings.get("healthEstimate", "starfinder.vehicleNames").split(/[,;]\s*/);
+		stage = Math.max(0, Math.ceil((descriptions.length - 1) * fraction));
+	} else if (state.dead) {
+		return state.desc;
+	}
+	return descriptions[stage];
+};
+
 const settings = () => {
 	return {
 		"core.addTemp": {
@@ -57,6 +64,8 @@ const settings = () => {
 	};
 };
 
-const breakCondition = `||game.settings.get('healthEstimate', 'core.breakOnZeroMaxHP') && token.actor.system.attributes.hp.max === 0`;
+const breakCondition = `
+	|| token.actor.type === 'hazard'
+	|| game.settings.get('healthEstimate', 'core.breakOnZeroMaxHP') && token.actor.system.attributes.hp.max === 0`;
 
 export { fraction, settings, descriptions, breakCondition };
