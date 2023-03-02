fc726f7435a075e9c34214c9a7c3642ace216815
Matheus Clemente <matheus.xb@gmail.com>
2023-03-02 00:54:52 -0300

    SWADE Incapacitated status (#105)
    Fix https://github.com/mclemente/healthEstimate/issues/105

--- module/systems/swade.js
+++ module/systems/swade.js
@@ -1,4 +1,4 @@
-import { descriptions, sGet, t } from "../utils.js";
+import { f, sGet, t } from "../utils.js";
 
 const fraction = function (token) {
 	const hp = token.actor.system.wounds;
@@ -8,6 +8,16 @@ const fraction = function (token) {
 		maxHP = 1 + Math.max(hp.max || defaultWildCardMaxWounds, 1);
 	}
 	return (maxHP - hp.value) / maxHP;
+};
+
+const descriptions = function (descriptions, stage, token, state = { dead: false, desc: "" }, fraction) {
+	if (token.actor.type == "vehicle") {
+		descriptions = game.settings.get("healthEstimate", "starfinder.vehicleNames").split(/[,;]\s*/);
+		stage = Math.max(0, Math.ceil((descriptions.length - 1) * fraction));
+	} else if (state.dead) {
+		return state.desc;
+	} else if (sGet("swade.showIncap") && token.actor.effects.find((e) => e._statusId == "incapacitated")) return game.i18n.localize("SWADE.Incap");
+	return descriptions[stage];
 };
 
 const tokenEffects = function (token) {
@@ -28,6 +38,12 @@ const settings = () => {
 			type: String,
 			default: t("swade.vehicleNames.default"),
 			hint: t("swade.vehicleNames.hint"),
+		},
+		"swade.showIncap": {
+			type: Boolean,
+			default: true,
+			name: f("swade.showIncap.name", { incap: game.i18n.localize("SWADE.Incap") }),
+			hint: f("swade.showIncap.hint", { incap: game.i18n.localize("SWADE.Incap") }),
 		},
 	};
 };
