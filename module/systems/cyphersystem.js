82fa08efd04306a2e1950e5bef0ffdceba3840f5
Martin <github@amusingtime.uk>
2023-03-28 23:33:46 +0100

    Better health estimate for cyphersystem (with user settings)

--- module/systems/cyphersystem.js
+++ module/systems/cyphersystem.js
@@ -1,31 +1,51 @@
 const fraction = function (token) {
     const actor = token.actor;
     if (actor.type === 'pc') {
+        const pools = actor.system.pools;
+        let curr = pools.might.value + pools.speed.value + pools.intellect.value;
+        let max  = pools.might.max   + pools.speed.max   + pools.intellect.max;
+        // TODO: should we ever include the additional pool in this calculation?
+        if (actor.system.settings.general.additionalPool.active) {
+            curr += pools.additional.value;
+            max  += pools.additional.max;
+        }
+        if (curr > max) curr = max;
+        let result = curr / max;
+        let limit = 1.0;
+
         switch (actor.system.combat.damageTrack.state) {
             case 'Hale':
-                {
-                    const pools = actor.system.pools;
-                    let curr = pools.might.value + pools.speed.value + pools.intellect.value;
-                    let max  = pools.might.max   + pools.speed.max   + pools.intellect.max;
-                    // TODO: should we ever include the additional pool in this calculation?
-                    if (actor.system.settings.general.additionalPool.active) {
-                        curr += pools.additional.value;
-                        max  += pools.additional.max;
-                    }
-                    if (curr > max) curr = max;
-                    return curr / max;
-                }
+                break;
             case 'Impaired':
-                return 0.66;
+                limit = game.settings.get('healthEstimate', "cyphersystem.impaired");
+                break;
             case 'Debilitated':
-                return 0.33;
+                limit = game.settings.get('healthEstimate', "cyphersystem.debilitated");
+                break;
             default:
-                return 0;
+                result = 0.0;
         }
-    } else {
+        if (result > limit) result = limit;
+        return result;
+    } else if (actor.system.pools?.health) {
         let hp = actor.system.pools.health;
         return hp.value / hp.max;
+    } else {
+        return ;
     }
 };
 
-export { fraction };
+const settings = () => {
+	return {
+		"cyphersystem.impaired": {
+			type: Number,
+			default: 0.5,
+		},
+		"cyphersystem.debilitated": {
+			type: Number,
+			default: 0.1,
+		},
+	};
+};
+
+export { fraction, settings };
