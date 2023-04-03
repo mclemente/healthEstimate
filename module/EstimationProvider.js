baf5cf99510d87e21f2ba617db94d54d31481539
Matheus Clemente <matheus.xb@gmail.com>
2023-04-02 22:21:49 -0300

    System-agnostic/Custom System Builder fixes

--- module/EstimationProvider.js
+++ module/EstimationProvider.js
@@ -116,11 +116,11 @@ export class GenericEstimationProvider extends EstimationProvider {
 		const hpPath = sGet("core.custom.FractionHP");
 		let hp = getNestedData(token, hpPath) || token.actor.system.attributes?.hp || token.actor.system.hp;
 		let temp = 0;
-		if (sGet("core.addTemp")) temp = hp?.temp || 0;
+		if (sGet("core.addTemp")) temp = Number(hp?.temp) || 0;
 
 		if (hp === undefined && hpPath === "") throw new Error(`The HP is undefined, try using the ${game.i18n.localize("healthEstimate.core.custom.FractionHP.name")} setting.`);
 		else if (hp === undefined) throw new Error(`The ${game.i18n.localize("healthEstimate.core.custom.FractionHP.name")} setting ("${hpPath}") is wrong.`);
-		const outputs = [Math.min((hp.value + temp) / hp.max, 1), (hp.max - hp.value) / hp.max];
+		const outputs = [Math.min((Number(hp.value) + temp) / Number(hp.max), 1), (Number(hp.max) - Number(hp.value)) / Number(hp.max)];
 		return outputs[sGet("core.custom.FractionMath")];
 	}
 
@@ -290,12 +290,12 @@ export class CustomSystemBuilderEstimationProvider extends EstimationProvider {
 		const hpPath = sGet("core.custom.FractionHP");
 		const hp = getNestedData(token, hpPath) || token.actor.system.attributes?.hp || token.actor.system.hp;
 		const thpPath = sGet("custom-system-builder.tempHP");
-		const temp = thpPath && token.actor.type === "character" ? getNestedData(token, thpPath).temp : 0;
+		const temp = thpPath && token.actor.type === "character" ? Number(getNestedData(token, thpPath).temp) : 0;
 
 		if (hp === undefined && hpPath === "") throw new Error(`The HP is undefined, try using the ${game.i18n.localize("healthEstimate.core.custom.FractionHP.name")} setting.`);
 		else if (hp === undefined) throw new Error(`The ${game.i18n.localize("healthEstimate.core.custom.FractionHP.name")} setting ("${hpPath}") is wrong.`);
 		else if (hp.max === undefined) throw new Error(`Token ${token.name}'s HP has no maximum value. Set it up if you intend for the estimation to work.`);
-		const outputs = [Math.min((hp.value + temp) / hp.max, 1), (hp.max - hp.value) / hp.max];
+		const outputs = [Math.min((Number(hp.value) + temp) / Number(hp.max), 1), (Number(hp.max) - Number(hp.value)) / Number(hp.max)];
 		return outputs[sGet("core.custom.FractionMath")];
 	}
 
