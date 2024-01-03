79cb8639f8d5cec2a97e91c6d42cc4951543d488
The_Narrator_In_Shadow <talkativestudent@gmail.com>
2024-01-03 18:39:57 -0500

    adding support for Level Up: Advanced Fifth Edition (A5E)

---
+++ src/module/providers/a5e.js
@@ -0,0 +1,19 @@
+import { getNestedData } from "../../TokenTooltipAlt.js";
+import { sGet, t } from "../../utils.js";
+import EstimationProvider from "./Base.js";
+
+export default class a5eEstimationProvider extends EstimationProvider {
+	/* 
+		Thanks to the original author of the 5E provider for basically providing the code needed to add A5E support.
+		Thanks to Nekro and Phil for using the same datapaths in A5E as what is used in 5E, making changes basically unnessary.
+	*/
+
+	fraction(token) {
+		const hp = token.actor.system.attributes.hp;
+		let temp = 0;
+		if (sGet("core.addTemp")) {
+			temp = hp.temp;
+		}
+		return Math.min((temp + hp.value) / hp.max, 1);
+	}
+}
