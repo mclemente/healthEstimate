4ce34d5153e102a62d0361b686886175f5d746ee
Matheus Clemente <matheus.xb@gmail.com>
2024-01-03 23:11:58 -0300

    Update a5e.js

--- src/module/providers/a5e.js
+++ src/module/providers/a5e.js
@@ -1,9 +1,8 @@
-import { getNestedData } from "../../TokenTooltipAlt.js";
-import { sGet, t } from "../../utils.js";
+import { sGet } from "../../utils.js";
 import EstimationProvider from "./Base.js";
 
 export default class a5eEstimationProvider extends EstimationProvider {
-	/* 
+	/*
 		Thanks to the original author of the 5E provider for basically providing the code needed to add A5E support.
 		Thanks to Nekro and Phil for using the same datapaths in A5E as what is used in 5E, making changes basically unnessary.
 	*/
