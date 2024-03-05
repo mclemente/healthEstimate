45995b1257d5b9231a0ac63bdea080fd807862f4
Matheus Clemente <matheus.xb@gmail.com>
2024-03-04 23:32:42 -0300

    Update dnd5e.js

--- src/module/providers/dnd5e.js
+++ src/module/providers/dnd5e.js
@@ -29,11 +29,10 @@ export default class dnd5eEstimationProvider extends EstimationProvider {
 
 	fraction(token) {
 		const hp = token.actor.system.attributes.hp;
-		let temp = 0;
 		if (token.actor.type === "character" && sGet("core.addTemp")) {
-			temp = hp.temp;
+			return Math.min(hp.pct/100, 1);
 		}
-		return Math.min((temp + hp.value) / hp.max, 1);
+		return Math.min(hp.value / hp.max, 1);
 	}
 
 	get breakCondition() {
