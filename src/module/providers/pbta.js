bdd24b6c9c73a66e9c99ea6490a6faa69dcef57e
Matheus Clemente <matheus.xb@gmail.com>
2024-08-07 18:26:34 -0300

    Update pbta.js
    Fix #255

--- src/module/providers/pbta.js
+++ src/module/providers/pbta.js
@@ -1,12 +1,21 @@
+import { sGet } from "../utils.js";
 import EstimationProvider from "./templates/Base.js";
 
 export default class pbtaEstimationProvider extends EstimationProvider {
 	fraction(token) {
-		const hp = token.actor.system.attrTop.hp
-			|| token.actor.system.attrLeft.hp
-			|| token.actor.system.attrTop.harm
-			|| token.actor.system.attrLeft.harm;
+		const hpPath = sGet("core.custom.FractionHP");
+		const hp = foundry.utils.getProperty(token, hpPath);
+		this._checkValidHP(token, hp, hpPath);
 		if (hp.type === "Resource") return hp.value / hp.max;
 		return (hp.max - hp.value) / hp.max;
+	}
+
+	get settings() {
+		return {
+			"core.custom.FractionHP": {
+				type: String,
+				default: ""
+			},
+		};
 	}
 }
