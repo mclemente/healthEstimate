2a7630c986de3a1db1d6199cfbda5c4881da0a46
Matheus Clemente <matheus.xb@gmail.com>
2024-08-26 19:48:40 -0300

    Update lancer.js
    Fix #259

--- src/module/providers/lancer.js
+++ src/module/providers/lancer.js
@@ -7,13 +7,13 @@ export default class lancerEstimationProvider extends EstimationProvider {
 	}
 
 	fraction(token) {
-		const hp = token.actor.system.derived.hp;
+		const hp = token.actor.system.hp;
 		return hp.value / hp.max;
 	}
 
 	get breakCondition() {
 		return `
         || game.settings.get('healthEstimate', 'core.breakOnZeroMaxHP')
-        && (token.actor.system.mech?.hp.max === 0 || token.actor.system?.derived?.hp?.max === 0)`;
+        && (token.actor.system?.hp?.max === 0)`;
 	}
 }
