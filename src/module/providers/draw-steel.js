d23ab52fefc6ca7256ed14cb17a0b49be53ea7b7
no <no@gmail.com>
2025-08-27 09:22:10 +0200

    Draw Steel support

---
+++ src/module/providers/draw-steel.js
@@ -0,0 +1,48 @@
+import EstimationProvider from "./templates/Base.js";
+
+export default class drawsteelEstimationProvider extends EstimationProvider {
+	constructor() {
+		super();
+		this.organicTypes = ["hero", "npc"];
+		this.breakOnZeroMaxHP = true;
+		this.estimations = [
+			{
+				name: "unconscious",
+				ignoreColor: true,
+				rule: "effects.values().some((x) => x.name === 'Unconscious' || x.name === 'Asleep');",
+				estimates: [{ value: 100, label: "Unconscious" }],
+			},
+			{
+				name: "Heroes",
+				rule: "token.actor.type === 'hero'",
+				estimates: [
+					{ value: 0, label: "Dead" },
+					{ value: 33, label: "Dying" },
+					{ value: 66, label: "Winded" },
+					{ value: 99, label: "Injured" },
+					{ value: 100, label: "Unharmed" },
+				],
+			},
+			{
+				name: "NPCs",
+				rule: "token.actor.type === 'npc'",
+				estimates: [
+					{ value: 0, label: "Dead" },
+					{ value: 25, label: "Near Death" },
+					{ value: 50, label: "Winded" },
+					{ value: 99, label: "Injured" },
+					{ value: 100, label: "Unharmed" },
+				],
+			},
+		];
+	}
+
+	fraction(token) {
+		const minHealth = token.actor.system.stamina.min;
+		const maxHealth = token.actor.system.stamina.max;
+		const currentHealth = token.actor.system.stamina.value;
+
+		const fraction = (currentHealth - minHealth) / (maxHealth - minHealth);
+		return Math.max(0, Math.min(1, fraction)); // clamp between 0–1
+	}
+}
