918ab93e74259844a3eb438f31faa7eb3050e7e1
Matheus Clemente <matheus.xb@gmail.com>
2025-09-25 15:37:16 -0300

    Update draw-steel.js

--- src/module/providers/draw-steel.js
+++ src/module/providers/draw-steel.js
@@ -12,7 +12,7 @@ export default class drawsteelEstimationProvider extends EstimationProvider {
 				rule: "",
 				estimates: [
 					{ value: 0, label: t("core.estimates.states.0") },
-					{ value: 33, label: "Dying" },
+					{ value: 33, label: game.i18n.localize("DRAW_STEEL.ActiveEffect.StaminaEffects.Dying") },
 					{ value: 66, label: game.i18n.localize("DRAW_STEEL.ActiveEffect.StaminaEffects.Winded") },
 					{ value: 99, label: t("core.estimates.states.4") },
 					{ value: 100, label: t("core.estimates.states.5") },
@@ -23,7 +23,7 @@ export default class drawsteelEstimationProvider extends EstimationProvider {
 				rule: "type === \"npc\"",
 				estimates: [
 					{ value: 0, label: t("core.estimates.states.0") },
-					{ value: 25, label: "Dying" },
+					{ value: 25, label: game.i18n.localize("DRAW_STEEL.ActiveEffect.StaminaEffects.Dying") },
 					{ value: 50, label: game.i18n.localize("DRAW_STEEL.ActiveEffect.StaminaEffects.Winded") },
 					{ value: 99, label: t("core.estimates.states.4") },
 					{ value: 100, label: t("core.estimates.states.5") },
